9a1059002d10ce6d0964a318f27f68189aab0851
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-11-12 12:21:12 -0600

    Add modelB

---
+++ models/modelB.js
@@ -0,0 +1,14 @@
+
+
+var house = {
+  city: "Mexico City",
+  neighborhood: "Roma Sur",
+  numBeds: 2,
+  numBaths: 2,
+  eatInKitchen: true,
+  patio: true,
+  features: ["hardwood floors", "open floor plan", "natural light"],
+  restrictions: ["no dogs over 50 lbs", "no short term rentals", "no loud music after 10PM"]
+
+};
+console.log(house);
