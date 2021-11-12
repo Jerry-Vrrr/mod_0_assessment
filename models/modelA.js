a90160dcd333fc4439334f7b9c1552bfc56b9579
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-11-12 12:13:13 -0600

    Add ModelA

---
+++ models/modelA.js
@@ -0,0 +1,13 @@
+
+
+var laptop = {
+  brand: "Apple",
+  model: "Macbook Air",
+  qwertyKeyboard: true,
+  internalSpeakers: true,
+  ageInYears: 1,
+  numberOfOwners: 1,
+  features: ["case", "trackpad", "13in screen"],
+  inputs: ["headphone jack", "USB-c", "USB-C"]
+};
+console.log(laptop);
