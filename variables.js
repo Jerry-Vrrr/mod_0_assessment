3f6d16e80320cbebe9da799327bf7309d70b5962
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-11-12 12:12:05 -0600

    Add variables

--- variables.js
+++ variables.js
@@ -1,1 +1,12 @@
-var test = true
+var animal = "crocodile";
+var isCuddly = false;
+animal = "lemur";
+
+var num8 = 16;
+var num3 = 6;
+var quotient = num8 / num3;
+
+var favoriteFilms = ["Raiders of the Lost Ark", "Leon: The Professional", "Roma"];
+favoriteFilms.shift();
+
+console.log(animal.length);
